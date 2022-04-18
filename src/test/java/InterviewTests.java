c2980e16baa97377265a762e48202c6bf4cf4978
adamchappell00 <adam.chappell00@gmail.com>
2022-04-18 15:01:36 -0500

    remove junit org for jupiter dependency

--- src/test/java/InterviewTests.java
+++ src/test/java/InterviewTests.java
@@ -1,18 +1,17 @@
 import Interview.DuplicateSolution;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
-
 
 public class InterviewTests {
 
     @Test
     public void testDuplicatesRemoved(){
         int[] nums = {1,1,2,2,3,3,3,4}; // Input array
-        int[] expectedNums = {1,2,3,4,1,2,3,3}; // The expected answer with correct length
+        int[] expectedNums = {1,2,3,4}; // The expected answer with correct length
 
         int k = DuplicateSolution.removeDuplicates(nums); // Calls your implementation
 
-        assertsame(k,expectedNums.length);
+        Assertions.assertSame(k,expectedNums.length);
         for (int i = 0; i < k; i++) {
             assert nums[i] == expectedNums[i];
         }
